c554f1cd59d1f4963fa9c5606a2dfb941af6c172
SaoDo <Dothisao070322@gmail.com>
2021-08-16 22:53:24 +0700

    Fifth commit

---
+++ src/com/javabasic/lesson7/Practice1.java
@@ -0,0 +1,49 @@
+package com.javabasic.lesson7;
+
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int N = enterNumber();
+        CheckPrimeNumber(N);
+        printf(N);
+    }
+
+
+
+    public static int enterNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number = ");
+        int number = scanner.nextInt();
+        return number;
+    }
+
+
+
+    public static boolean CheckPrimeNumber(int N) {
+        if (N < 2) {
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(N);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public static void printf(int N) {
+        System.out.printf("All primes less than N are: ", N);
+        if (N >= 2) {
+            System.out.print(2);
+        }
+        for (int i = 3; i < N; i += 2) {
+            if (CheckPrimeNumber(i)) {
+                System.out.print(" " + i);
+            }
+        }
+    }
+}
---
+++ src/com/javabasic/lesson7/Practice4.java
@@ -0,0 +1,54 @@
+package com.javabasic.lesson7;
+
+import java.util.Scanner;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        int number1 = enterNumber();
+        CheckPrimeNumber(number1);
+        printf(number1);
+    }
+
+
+    public static int enterNumber(){
+        System.out.println("Enter number : ");
+        Scanner scanner = new Scanner(System.in);
+        int Number = scanner.nextInt();
+        return Number;
+    }
+
+
+
+    public static boolean CheckPrimeNumber(int number1) {
+        if (number1 < 2) {
+
+            return false;
+        }
+        int squareRoot = (int) Math.sqrt(number1);
+
+        for (int i = 2; i <= squareRoot; i++) {
+            if (number1 % i == 0) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+
+    public static void printf(int number1){
+        System.out.printf("%d first prime number is : \n", number1);
+        int dem = 0;
+        int i = 2;
+        while (dem < number1) {
+            if (CheckPrimeNumber(i)) {
+                System.out.print(i + " ");
+                dem++;
+            }
+            i++;
+        }
+    }
+}
+
